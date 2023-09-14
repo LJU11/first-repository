58b58a8dd998b5e2e471c63cff62f834d95abf8d
LJU11 <wodn30@gmail.com>
2023-09-14 11:13:54 +0900

    Delete hello_world.py
    Delete

--- hello_world.py
+++
@@ -1,9 +0,0 @@
-def print_hello():
-    print("Hello World!!")
-
-def print_KIT():
-    print("kumoh National Institute of Techonology.")
-if __name__ == '__main__':
-    print_KIT()
-    print_hello()
-    
