043249c63b792c1fb9a064e01fcbb73e0fdafa60
LJU11 <wodn30@gmail.com>
2023-09-14 11:09:37 +0900

    Update hello_world.py
    Revice hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,9 @@
 def print_hello():
     print("Hello World!!")
 
+def print_KIT():
+    print("kumoh National Institute of Techonology.")
 if __name__ == '__main__':
+    print_KIT()
     print_hello()
     
