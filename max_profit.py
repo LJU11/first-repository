540522678fdddfc5f327a6e0d5bf9e6c4ac148e5
LJU11 <wodn30@gmail.com>
2023-09-14 11:30:38 +0900

    revice

--- max_profit.py
+++ max_profit.py
@@ -4,6 +4,6 @@ def maxProfit_bruteforce (prices):
    for i, price in enumerate(prices):
        for j in range(i, len(prices)):
            max_price = max(prices[j] - price, max_price)
-
+    #MAXPROFIT
    return
 
